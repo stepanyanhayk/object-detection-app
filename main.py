e432f0d8d321ffa8def6079a5320f15626128dfe
stepanyanhayk <haykstepanyan02@gmail.com>
2020-11-25 16:41:53 +0400

    modified main.py

--- main.py
+++ main.py
@@ -127,8 +127,7 @@ configs = config_util.get_configs_from_pipeline_file(pipeline_config)
 model_config = configs['model']
 model_config.ssd.num_classes = num_classes
 model_config.ssd.freeze_batchnorm = True
-detection_model = model_builder.build(
-      model_config=model_config, is_training=True)
+detection_model = model_builder.build(model_config=model_config, is_training=True)
 
 # Set up object-based checkpoint restore --- RetinaNet has two prediction
 # `heads` --- one for classification, the other for box regression.  We will
@@ -168,52 +167,50 @@ num_batches = 25
 trainable_variables = detection_model.trainable_variables
 to_fine_tune = []
 prefixes_to_train = [
-  'WeightSharedConvolutionalBoxPredictor/WeightSharedConvolutionalBoxHead',
-  'WeightSharedConvolutionalBoxPredictor/WeightSharedConvolutionalClassHead']
+    'WeightSharedConvolutionalBoxPredictor/WeightSharedConvolutionalBoxHead',
+    'WeightSharedConvolutionalBoxPredictor/WeightSharedConvolutionalClassHead']
 for var in trainable_variables:
-  if any([var.name.startswith(prefix) for prefix in prefixes_to_train]):
-    to_fine_tune.append(var)
+    if any([var.name.startswith(prefix) for prefix in prefixes_to_train]):
+        to_fine_tune.append(var)
 
 # Set up forward + backward pass for a single train step.
 def get_model_train_step_function(model, optimizer, vars_to_fine_tune):
-  """Get a tf.function for training step."""
+    """Get a tf.function for training step."""
 
-  # Use tf.function for a bit of speed.
-  # Comment out the tf.function decorator if you want the inside of the
-  # function to run eagerly.
-  @tf.function
-  def train_step_fn(image_tensors,
+    # Use tf.function for a bit of speed.
+    # Comment out the tf.function decorator if you want the inside of the
+    # function to run eagerly.
+    @tf.function
+    def train_step_fn(image_tensors,
                     groundtruth_boxes_list,
                     groundtruth_classes_list):
-    """A single training iteration.
-    Args:
-      image_tensors: A list of [1, height, width, 3] Tensor of type tf.float32.
-        Note that the height and width can vary across images, as they are
-        reshaped within this function to be 640x640.
-      groundtruth_boxes_list: A list of Tensors of shape [N_i, 4] with type
-        tf.float32 representing groundtruth boxes for each image in the batch.
-      groundtruth_classes_list: A list of Tensors of shape [N_i, num_classes]
-        with type tf.float32 representing groundtruth boxes for each image in
-        the batch.
-    Returns:
-      A scalar tensor representing the total loss for the input batch.
-    """
-    shapes = tf.constant(batch_size * [[640, 640, 3]], dtype=tf.int32)
-    model.provide_groundtruth(
-        groundtruth_boxes_list=groundtruth_boxes_list,
-        groundtruth_classes_list=groundtruth_classes_list)
-    with tf.GradientTape() as tape:
-      preprocessed_images = tf.concat(
-          [detection_model.preprocess(image_tensor)[0]
-           for image_tensor in image_tensors], axis=0)
-      prediction_dict = model.predict(preprocessed_images, shapes)
-      losses_dict = model.loss(prediction_dict, shapes)
-      total_loss = losses_dict['Loss/localization_loss'] + losses_dict['Loss/classification_loss']
-      gradients = tape.gradient(total_loss, vars_to_fine_tune)
-      optimizer.apply_gradients(zip(gradients, vars_to_fine_tune))
-    return total_loss
-
-  return train_step_fn
+        """A single training iteration.
+        Args:
+        image_tensors: A list of [1, height, width, 3] Tensor of type tf.float32.
+            Note that the height and width can vary across images, as they are
+            reshaped within this function to be 640x640.
+        groundtruth_boxes_list: A list of Tensors of shape [N_i, 4] with type
+            tf.float32 representing groundtruth boxes for each image in the batch.
+        groundtruth_classes_list: A list of Tensors of shape [N_i, num_classes]
+            with type tf.float32 representing groundtruth boxes for each image in
+            the batch.
+        Returns:
+        A scalar tensor representing the total loss for the input batch.
+        """
+        shapes = tf.constant(batch_size * [[640, 640, 3]], dtype=tf.int32)
+        model.provide_groundtruth(
+            groundtruth_boxes_list=groundtruth_boxes_list,
+            groundtruth_classes_list=groundtruth_classes_list)
+        with tf.GradientTape() as tape:
+            preprocessed_images = tf.concat([detection_model.preprocess(image_tensor)[0]
+            for image_tensor in image_tensors], axis=0)
+            prediction_dict = model.predict(preprocessed_images, shapes)
+            losses_dict = model.loss(prediction_dict, shapes)
+            total_loss = losses_dict['Loss/localization_loss'] + losses_dict['Loss/classification_loss']
+            gradients = tape.gradient(total_loss, vars_to_fine_tune)
+            optimizer.apply_gradients(zip(gradients, vars_to_fine_tune))
+        return total_loss
+    return train_step_fn
 
 optimizer = tf.keras.optimizers.SGD(learning_rate=learning_rate, momentum=0.9)
 train_step_fn = get_model_train_step_function(
@@ -221,25 +218,22 @@ train_step_fn = get_model_train_step_function(
 
 print('Start fine-tuning!', flush=True)
 for idx in range(num_batches):
-  # Grab keys for a random subset of examples
-  all_keys = list(range(len(train_images_np)))
-  random.shuffle(all_keys)
-  example_keys = all_keys[:batch_size]
+    # Grab keys for a random subset of examples
+    all_keys = list(range(len(train_images_np)))
+    random.shuffle(all_keys)
+    example_keys = all_keys[:batch_size]
 
-  # Note that we do not do data augmentation in this demo.  If you want a
-  # a fun exercise, we recommend experimenting with random horizontal flipping
-  # and random cropping :)
-  gt_boxes_list = [gt_box_tensors[key] for key in example_keys]
-  gt_classes_list = [gt_classes_one_hot_tensors[key] for key in example_keys]
-  image_tensors = [train_image_tensors[key] for key in example_keys]
+    # Note that we do not do data augmentation in this demo.  If you want a
+    # a fun exercise, we recommend experimenting with random horizontal flipping
+    # and random cropping :)
+    gt_boxes_list = [gt_box_tensors[key] for key in example_keys]
+    gt_classes_list = [gt_classes_one_hot_tensors[key] for key in example_keys]
+    image_tensors = [train_image_tensors[key] for key in example_keys]
 
-  # Training step (forward pass + backwards pass)
-  total_loss = train_step_fn(image_tensors, gt_boxes_list, gt_classes_list)
+    # Training step (forward pass + backwards pass)
+    total_loss = train_step_fn(image_tensors, gt_boxes_list, gt_classes_list)
 
-  if idx % 2 == 0:
-    print('batch ' + str(idx) + ' of ' + str(num_batches)
-    + ', loss=' +  str(total_loss.numpy()), flush=True)
-
+    if idx % 2 == 0:
+        print('batch ' + str(idx) + ' of ' + str(num_batches)
+        + ', loss=' +  str(total_loss.numpy()), flush=True)
 print('Done fine-tuning!')
-
-
